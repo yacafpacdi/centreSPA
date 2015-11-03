95c0e363c7f6a4d7a5737803f088b6999098c4bc
cdi403 <cdi403@pltz888.AFPA-PARIS-DI>
2015-11-03 16:46:31 +0100

    Ajout de Outils Ajout de RequetesJPQL

---
+++ src/requetes/Outils.java
@@ -0,0 +1,16 @@
+package requetes;
+
+import java.util.List;
+
+
+public class Outils<T> {
+    
+    public void afficher(List<T> list){
+        for (T t : list) {
+            System.out.println(t);
+            
+        }
+        
+    }
+    
+}
---
+++ src/requetes/RequetesJPQL.java
@@ -0,0 +1,102 @@
+package requetes;
+
+import entites.Animal;
+import entites.Soigneur;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+public class RequetesJPQL {
+
+    public static void main(String[] args) {
+        System.out.println("=================DEBUT=======================");
+
+        //pour obtenir l'entityManager
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("centreSPAPU");
+        EntityManager em = emf.createEntityManager();
+        Outils<Soigneur> os = new Outils<>();
+        Outils<Animal> oa = new Outils<>();
+
+        System.out.println("1) tous les soigneurs");
+        String req = "select s from Soigneur s";
+        Query qr = em.createQuery(req);
+        List<Soigneur> ls = qr.getResultList();
+        os.afficher(ls);
+        System.out.println(">>>>> date de naissance de ls(0) ");
+        System.out.println(ls.get(0).getNaissance());
+
+        String race = "chien";
+        System.out.println("2) tous les animaux de race = " + race);
+        req = "select a from Animal a where a.race = :paramRace";
+        qr = em.createQuery(req);
+        qr.setParameter("paramRace", race);
+        List<Animal> la = qr.getResultList();
+        oa.afficher(la);
+
+        System.out.println("3) tous les soigneurs qui soignent un " + race);
+        req = "select distinct a.soigneurs from Animal a where a.race= :paramRace"; // distinct pour éliminer les doublons
+        qr = em.createQuery(req);
+        qr.setParameter("paramRace", race);
+        ls = qr.getResultList();
+        os.afficher(ls);
+        //System.out.println(">>>>> qr = " + qr + "\n");
+
+        Integer age = 1;
+        System.out.println("4) tous les animaux qui ont au moins  " + age + " ans");
+        req = "select a from Animal a where a.age >= :paramAge";
+        qr = em.createQuery(req);
+        qr.setParameter("paramAge", age);
+        //pagination
+//        int debut = 2;
+//        int qteParPage =2;
+//        qr.setFirstResult(debut);//premier élément à afficher 
+//        qr.setMaxResults(qteParPage);// nombre d'éléments dans la page
+        la = qr.getResultList();
+        oa.afficher(la);
+
+        
+        Long id = 3L;
+        System.out.println("5) tous les animaux d'un soigneur id = "+id);
+        System.out.println("5.1) avec un objet");
+        Soigneur s01 = em.find(Soigneur.class, id);
+        req = "select s.animaux from Soigneur s where s= :paramSoigneur";
+        qr = em.createQuery(req);
+        qr.setParameter("paramSoigneur", s01);
+        la = qr.getResultList();
+        oa.afficher(la);
+        
+        System.out.println("5.2) avec l'id");
+        req = "select s.animaux from Soigneur s where s.id= :paramId";
+        qr = em.createQuery(req);
+        qr.setParameter("paramId", id);
+        la = qr.getResultList();
+        oa.afficher(la);
+        
+        int qte=1;
+        System.out.println("6) tous les soigneurs qui soignent au moins " + qte+ " animaux");
+        req = "select distinct s from Soigneur s where SIZE(s.animaux) >= :paramQte"; // distinct pour éliminer les doublons
+        qr = em.createQuery(req);
+        System.out.println(">>>>> qr = " + qr + "\n");
+        qr.setParameter("paramQte", qte);
+        ls = qr.getResultList();
+        os.afficher(ls);
+        
+        id = 3L;
+        System.out.println("7) tous les soigneurs qui travaillent le centre id =  " + id);
+        req = "select a.soigneurs from Animal a where a.centre.id = :paramId"; // distinct pour éliminer les doublons
+        qr = em.createQuery(req);
+        System.out.println(">>>>> qr = " + qr + "\n");
+        qr.setParameter("paramId", id);
+        ls = qr.getResultList();
+        os.afficher(ls);
+        
+        
+        em.close();
+        emf.close();
+        System.out.println("=================FIN=======================");
+
+    }
+
+}
